a2fbcbd8db7cf4d09d479c3580ff1f692ec145ff
rahul <cmrohityadav23@gmail.com>
2023-01-20 22:48:30 +0530

    reverse number trying

---
+++ Basic Maths/02_reverseNumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin>>num;
+    int rev=0;
+    while (num!=0)
+    {
+       int lastDigit=
+    }
+    
+    return 0;
+}
