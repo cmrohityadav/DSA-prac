8219812bdb5ccf82d3b22cb058660b36d4d4689d
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-02-07 19:13:23 +0530

    example

---
+++ Cpp/ScopeOf Variable/03_.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=5;
+    cout<<a<<endl;
+     int b=2;
+    cout<<b<<endl;
+    {
+        int a=6;
+        b=b+2;
+        
+    }
+  cout<<a<<endl;
+  cout<<b<<endl;
+    return 0;
+}
