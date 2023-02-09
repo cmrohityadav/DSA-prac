5ebb2fb933c79216c92aceb3d7e94942f645395a
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-02-09 17:51:40 +0530

    linear search

--- Array/demo.cpp
+++ Array/demo.cpp
@@ -5,12 +5,15 @@ int main()
 {
     
     int arr[5]={1,2,3,0,-1};
-    int max=arr[0];
-    for(int i=0; i<5;i++){
-        if(arr[i]>max){
-            max=arr[i];
+    int ans=-1;
+    int key;
+    cin>>key;
+    for(int i=0;i<5;i++){
+        if(arr[i]==key){
+        ans=i;
+
         }
     }
-cout<<"max value is the "<<max;
+    cout<<ans;
     return 0;
 }
