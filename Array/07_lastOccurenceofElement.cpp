88cd0f3056eeadf1fbd4847154ce846bbe9f9736
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-02-10 18:01:23 +0530

    2nd method for last occurence in element

--- Array/07_lastOccurenceofElement.cpp
+++ Array/07_lastOccurenceofElement.cpp
@@ -12,9 +12,18 @@ int main()
     int x;
     cin>>x;
     int occurence=-1;
+    //1ts method
     for(int i=0;i<v.size();i++){
         if(v[i]==x){
             occurence=i;
+        }
+    }
+
+//2nd method
+ for(int i=v.size()-1;i>=0;i--){
+        if(v[i]==x){
+            occurence=i;
+            break;
         }
     }
 
