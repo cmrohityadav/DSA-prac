4f73584df6dc2379ee1de4ca4f7107c60ea427a6
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-02-12 19:22:48 +0530

    secondLargestElement second method

--- Array/15_findSecondLargestElement.cpp
+++ Array/15_findSecondLargestElement.cpp
@@ -12,15 +12,41 @@ int largestElementIndex(int array[],int size){
     }
     return maxIndex;
 }
+int secondLargestElement(int array[],int size);
 int main()
 {
     
-int array[]={2,3,5,7,6,1};
+// int array[]={2,3,5,7,6,1};
 
-int indexOfLargest=largestElementIndex(array,6);
-array[indexOfLargest]=-1;
-int indexOfSecondLargestElement=largestElementIndex(array,6);
-cout<<array[indexOfSecondLargestElement];
+// int indexOfLargest=largestElementIndex(array,6);
+// array[indexOfLargest]=-1;
+// int indexOfSecondLargestElement=largestElementIndex(array,6);
+// cout<<array[indexOfSecondLargestElement];
+
+
+
+//second method
+int array[]={2,3,5,7,6,1,7};
+
+cout<<secondLargestElement(array,7);
 
     return 0;
+}
+int secondLargestElement(int array[],int size){
+
+    int max=INT_MIN;
+    int secondMax=INT_MIN;
+    for(int i=0;i<size;i++){
+        if(array[i]>max){
+            max=array[i];
+        }
+    }
+
+    for(int i=0;i<size;i++){
+        if(array[i]>secondMax && array[i]!=max){
+             secondMax=array[i];
+        }
+    }
+
+    return secondMax;
 }
