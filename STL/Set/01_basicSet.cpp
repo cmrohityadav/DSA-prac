a76ce1599546b99f10b667e3190d6d07fff43db8
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-02-16 14:47:48 +0530

    basics of set in stl

---
+++ STL/Set/01_basicSet.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+int main()
+{   
+    set<int>s;
+    
+    s.insert(5);
+    s.insert(5);
+    s.insert(5);
+    s.insert(1);
+    s.insert(1);
+    s.insert(6);
+    s.insert(0);
+    s.insert(0);
+    s.insert(0);
+    s.insert(0);
+    s.insert(0);
+
+    for(auto i:s){
+        cout<<i<<endl; //set duplicate element ko print nhi krta hai
+
+    }
+    cout<<endl;
+    s.erase(s.begin());
+
+    for(auto i:s){
+        cout<<i<<endl; 
+
+    }
+    cout<<endl;
+    cout<<" 0 present or not =>>"<<s.count(6)<<endl;
+    cout<<" 10 present or not =>>"<<s.count(10)<<endl;
+
+    
+    return 0;
+}
