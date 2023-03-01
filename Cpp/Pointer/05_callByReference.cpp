1243806c3da581e4514b3947c74eaf65e565ecbc
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-03-01 18:07:32 +0530

    pointer reference

---
+++ Cpp/Pointer/05_callByReference.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void swap(int *x,int *y){
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+int main()
+{
+    int x=10;
+    int y=20;
+    int *p1=&x;
+    int *p2=&y;
+    cout<<x<<"  "<<y<<endl;
+    swap(p1,p2);
+    cout<<x<<"  "<<y<<endl;
+    
+    return 0;
+}
