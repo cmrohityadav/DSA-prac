67a6d637f382a81660b12ae7b52b28da657b5c29
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-03-18 15:51:34 +0530

    strings basics

---
+++ String/01_theory.md
@@ -0,0 +1,2 @@
+# object of a class std:string in c++
+# used to represent the sequence of characters
---
+++ String/02_basicsOfstring.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+
+ string str="rohit";
+ cout<<str<<endl;
+
+ string str1("hii boys");
+
+ cout<<str1<<endl;
+
+
+
+ string str3;
+ cin>>str3;
+ cout<<str3<<endl;
+
+
+ string multipleline;
+ getline(cin,multipleline);
+ cout<<multipleline<<endl;
+ 
+    return 0;
+}
