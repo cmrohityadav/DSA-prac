525703d35c4646ee4d51da81df838df9d9e5f827
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-04-01 13:21:17 +0530

    reverse

---
+++ STL/Inbuild functions/07_reverse.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0; i<n;i++){
+        cin>>v[i];
+    }
+    
+    reverse(v.begin(),v.end());
+    for(int i=0; i<n;i++){
+        cout<<v[i];
+    }
+  cout<<endl;
+
+  //string
+  string name="Rohit";
+  reverse(name.begin(),name.end());
+  cout<<name<<endl;
+
+
+
+    
+    
+    return 0;
+}
