0ecd2ec70ddb585fcfbf73da34130ca26261cf23
cmrohityadav <cmrohityadav23@gmail.com>
2023-06-26 18:22:54 +0530

    constructor

---
+++ OOPS/04_constructor.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+class rect
+{
+public:
+    int l;
+    int b;
+
+    rect()
+    { // default constructor -> no args passed
+        l = 0;
+        b = 0;
+    }
+
+    rect(int x, int y)
+    { // parameterised constructor -> args pass
+        l = x;
+        b = y;
+    }
+
+    rect(rect & r) //copy constructor -> initialize an object by another exiting object
+    {
+        l=r.l;
+        b=r.b;
+    }
+};
+
+int main()
+{
+
+    rect r1;
+    cout << r1.l << " <--> " << r1.b << endl;
+
+    rect r2(1, 2);
+    cout << r2.l << " <--> " << r2.b << endl;
+
+    rect r3=r2;
+    cout<<r3.l<<" <--> "<<r3.b<<endl;
+
+    return 0;
+}
