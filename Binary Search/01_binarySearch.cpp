d921c7399922fe041d2858bcc1562afc3431cc8f
cmrohityadav <cmrohityadav23@gmail.com>
2023-07-12 15:20:48 +0530

    binary search

---
+++ Binary Search/01_binarySearch.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+
+//function
+
+int binarySearch(int arr[],int size,int key){
+
+    int start=0;
+    int end =size-1;
+    // int mid=(start+end)/2;
+    int mid=start+(end-start)/2;
+
+    while (start<=end)
+    {
+       if (arr[mid]==key)
+       {
+        return mid;
+       } 
+
+       //go to right part
+       if(key>arr[mid]){
+        start=mid+1;
+
+       }else{
+        end=mid-1; //for left part
+       }
+
+    //updating mid
+    // int mid=(start+end)/2;
+    int mid=start+(end-start)/2;
+
+ }
+ return -1;
+    
+}
+
+int main()
+{
+    int even[6]={2,4,6,8,12,18};
+    int odd[5]={2,4,6,8,12};
+
+int indexOdd=binarySearch(odd,5,2);
+
+cout<<"Index of 2 is "<<indexOdd<<endl;
+
+
+
+    return 0;
+}
