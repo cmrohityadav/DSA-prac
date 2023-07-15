ec03240feb17620a4f667623d893b60be824546a
cmrohityadav <cmrohityadav23@gmail.com>
2023-07-15 14:53:22 +0530

    test

--- test.cpp
+++ test.cpp
@@ -5,17 +5,8 @@ using namespace std;
  
 int main()
 {
-    int num=100;
-    int log=log10(num);
-    cout<<log<<endl;
-    cout<<endl;
-
-    
-    long long int ans = 0 ;
-        for(long long int i=1 ; i<=num ; i++){
-            ans += log10(i) + 1;
-            cout<<"log"<<"("<<i<<") :"<<log10(i)<<endl;
-            cout<<ans<<endl;
-        }
+  
+  int num=log10(100);
+  cout<<num;
     return 0;
 }
