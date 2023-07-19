644fd3a2bc73b93117fef9f92845d971d3f229b6
cmrohityadav <cmrohityadav23@gmail.com>
2023-07-19 16:29:03 +0530

    unordered multimap

--- STL/Map/13_unorderedMultimap.cpp
+++ STL/Map/13_unorderedMultimap.cpp
@@ -5,6 +5,14 @@ using namespace std;
 
 int main()
 {
+    unordered_multimap<string,int>fruitCnt;
+    fruitCnt.insert(make_pair("apple",10));
+    fruitCnt.insert(make_pair("mango",10));
+    fruitCnt.insert(make_pair("apple",9));
+
+    for(auto itr:fruitCnt){
+        cout<<itr.first<<" "<<itr.second<<endl;
+    }
     
 
 
