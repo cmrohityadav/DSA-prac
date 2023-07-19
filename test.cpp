5fa542db6a6cb19243fd70a459df5a41d3b78292
cmrohityadav <cmrohityadav23@gmail.com>
2023-07-19 12:50:58 +0530

    Check if Array Is Sorted and Rotated

--- test.cpp
+++ test.cpp
@@ -1,20 +1,8 @@
 #include<iostream>
-#include<cmath>
 using namespace std;
 
- 
 int main()
 {
-  
-   int i=0;
-   while(i<100){
-    if(i==50) break;
-    i++;
-
-
-   }
-
-   cout<<i;
-
+    
     return 0;
 }
