4655725dbaa8e4cf5b574c331e2ff4168c9eda45
cmrohityadav <cmrohityadav23@gmail.com>
2023-07-24 01:33:40 +0530

    string function

---
+++ String/11_inbuildFunctionString.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    
+
+    string name="rohit";
+    int len=name.length();
+    cout<<len<<endl;
+    return 0;
+}
