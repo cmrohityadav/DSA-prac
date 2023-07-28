3490e16fbd2d9e8183054e983e129ca0aa4a45d2
cmrohityadav <cmrohityadav23@gmail.com>
2023-07-28 14:31:43 +0530

    pointer

--- Cpp/Pointer/08_pointer.cpp
+++ Cpp/Pointer/08_pointer.cpp
@@ -5,10 +5,11 @@ using namespace std;
 int main(){
 
 
-    int arr[2]={1,2};
+    int arr[2]={1,3};
     int *ptr=&arr[0];
     cout<<ptr<<"  "<<*ptr<<endl;
     cout<<ptr+1<<"  "<<*(ptr+1)<<endl;
+    cout<<ptr<<"  "<<*(ptr)+1<<endl;
 
 
     return 0;
--- test.cpp
+++ test.cpp
@@ -3,23 +3,9 @@ using namespace std;
 
 int main()
 {
-   int a=5;
-
-   cout<<"value of a "<<a<<endl;
-    cout<<"address of a normally  "<<&a<<endl;
-
-   int *prt=&a;
-   cout<<"address of a using pointer "<<prt<<endl;
-
-   cout<<"dereference "<<*prt<<endl;
-
-   int *q=0;
-   q=&a;
-   cout<<q<<" r"<<*q<<endl;
- cout<<"address of a using pointer "<<q<<endl;
-
-
-
+  int arr[5]={1,2,4,5,6};
+  arr=arr+1;
+  
     
     return 0;
 }
