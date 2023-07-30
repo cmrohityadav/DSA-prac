f1d0c8fd8fb1a4da6b52fe742b9bcc5ac791277d
cmrohityadav <cmrohityadav23@gmail.com>
2023-07-30 21:18:13 +0530

    macro

---
+++ Cpp/Macro/01_macro.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#define pi 3.14
+
+using namespace std;
+
+int main()
+{
+    double r;
+    cout<<"Enter the radius of circle"<<endl;
+    cin>>r;
+    double area =pi*r*r;
+
+    cout<<"Area of the circle "<<area<<endl;
+    return 0;
+}
