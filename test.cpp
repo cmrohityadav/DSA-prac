fc0d1d0415622ba3c9f8505c1db29b6e9c299a05
cmrohityadav <cmrohityadav23@gmail.com>
2023-09-03 02:13:25 +0530

    test

--- test.cpp
+++ test.cpp
@@ -3,17 +3,10 @@ using namespace std;
 
 int main()
 {
- cout<<"enter the number";
- int num;
- cin>>num;
-int fact=1;
-for(int i=1;i<=num;i++){
+   
 
-   fact*=i;
-
-}
-
-cout<<"factorial"<<fact<<endl;
 
     return 0;
 }
+
+
