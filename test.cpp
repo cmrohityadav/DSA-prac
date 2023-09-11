470a21014fc7009b35e57c4c5c45c847c8b69788
cmrohityadav <cmrohityadav23@gmail.com>
2023-09-11 20:15:30 +0530

    live lec

--- test.cpp
+++ test.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
-#include<string>
 using namespace std;
 
 int main()
-{ 
+{
+  
    
+  int amoutRes;
+int gst;
+cout<<"enter your amout"<<"  enter gst"<<endl;
+  cin>>amoutRes>>gst;
+  cout<<"total amout"<<endl;
+  cout<<amoutRes+gst<<endl;
+
+
+
    
-
-
-
     return 0;
 }
-
-
