ca47264f44dbb99d9135ca228b1d77df2641b1ec
cmrohityadav <cmrohityadav23@gmail.com>
2023-09-13 20:53:41 +0530

    odd even

--- test.cpp
+++ test.cpp
@@ -4,21 +4,19 @@ using namespace std;
 int main()
 {
   
- cout<<"voting system check your elibility"<<endl;
- int age;
- cout<<"Enter your age"<<endl;
- cin>>age;
+ 
+cout<<"odd and even number"<<endl;
 
- if(age>18){
-    cout<<"you can vote";
- }else if(age<18){
-    cout<<"wait for 1 year ";
- }else if(age<15){
-    cout<<" your child";
- }else{
-    cout<<"not eligible";
- }
+int number;
+cin>>number;
 
+if(number%2==0){
+    cout<<"even is "<<number;
+
+}else{
+    cout<<" odd is  "<<number;
+
+}
 
 
 
