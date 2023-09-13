c8a4472e7796bddf7ae9cccaedcd5e7604cb910e
cmrohityadav <cmrohityadav23@gmail.com>
2023-09-13 19:13:15 +0530

    voting

--- test.cpp
+++ test.cpp
@@ -4,13 +4,23 @@ using namespace std;
 int main()
 {
   
-   
-  int amoutRes;
-int gst;
-cout<<"enter your amout"<<"  enter gst"<<endl;
-  cin>>amoutRes>>gst;
-  cout<<"total amout"<<endl;
-  cout<<amoutRes+gst<<endl;
+ cout<<"voting system check your elibility"<<endl;
+ int age;
+ cout<<"Enter your age"<<endl;
+ cin>>age;
+
+ if(age>18){
+    cout<<"you can vote";
+ }else if(age<18){
+    cout<<"wait for 1 year ";
+ }else if(age<15){
+    cout<<" your child";
+ }else{
+    cout<<"not eligible";
+ }
+
+
+
 
 
 
