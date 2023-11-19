3ea416e99aa00c10ec02b5c4cbbe922416bfe9b7
cmrohityadav <cmrohityadav23@gmail.com>
2023-11-19 11:37:28 +0530

    06_hollowPyramid

---
+++ Patterns/06_hollowPyramid.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{   
+
+    int row;
+    cin>>row;
+
+    for(int i=1;i<=row;i++){
+
+        for(int s=1;s<=(row-i);s++){
+            cout<<" ";
+
+        }
+
+        for(int j=1;j<=(2*i-1);j++){
+
+            if(j==1 || j==2*i-1 || i==row)
+             cout<<"*";
+             else 
+             cout<<" ";
+        }
+
+        cout<<endl;
+    }
+    
+    return 0;
+}
