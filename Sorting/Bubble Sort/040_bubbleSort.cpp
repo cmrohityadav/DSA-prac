73e76eb7b3f2d32c0a10c59ac20995c8b5f80bde
cmrohityadav <cmrohityadav23@gmail.com>
2023-12-04 20:55:24 +0530

    bubble sort

---
+++ Sorting/Bubble Sort/040_bubbleSort.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+#include<vector>
+using namespace std;
+
+int main()
+{
+    
+
+    vector<int>v{0,7,8,9,6,54,5};
+
+    int n=v.size();
+
+    for(int i=0;i<n-1;i++){
+
+        for(int j=0;j<n-1-i;j++){
+
+            if(v[j]>v[j+1]){
+
+                swap(v[j],v[j+1]);
+            }
+        }
+    }
+
+
+
+    for(auto i:v){
+
+        cout<<i<<" ";
+    }
+
+
+
+    return 0;
+}
