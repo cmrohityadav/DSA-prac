59d86d12c473b969b494ed344a3da0ea858dc9d4
cmrohityadav <cmrohityadav23@gmail.com>
2023-12-14 16:51:21 +0530

    pointer

--- Cpp/Pointer/03_pointer.cpp
+++ Cpp/Pointer/03_pointer.cpp
@@ -3,11 +3,17 @@ using namespace std;
 
 int main()
 {   
+
+    //bad practice
     int *ptr;
     int mark=10;
 
     ptr=&mark;
     cout<<ptr;
+
+    //null pointer
+    int *prtn=nullptr;
+    cout<<prtn<<endl;
     
     return 0;
 }
