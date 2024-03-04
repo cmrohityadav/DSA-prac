c115616984673bc96500d565c6babc9b1153458a
cmrohityadav <cmrohityadav23@gmail.com>
2024-03-04 14:38:17 +0530

    pRaiseToQ

---
+++ Recursion/04_pRaiseToQ.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+int pRaiseToQ(int p,int q){
+
+    if(q==0) return  1;
+
+    return pRaiseToQ(p,--q)*p;
+}
+int main(){
+
+
+        int result=pRaiseToQ(3,2);
+        cout<<result<<endl;
+
+    return 0;
+}
