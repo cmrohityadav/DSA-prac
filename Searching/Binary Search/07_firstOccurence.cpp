6bff6c4dd102535a627cd6fa6c81e7df0f283861
cmrohityadav <cmrohityadav23@gmail.com>
2024-03-13 19:39:59 +0530

    firstOccurence using BS

--- Searching/Binary Search/07_firstOccurence.cpp
+++ Searching/Binary Search/07_firstOccurence.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
 using namespace std;
-int firstOccurence(){
-    
+int firstOccurence(int arr[],int size,int target){
+     int start=0;
+     int end=size-1;
+     int mid=start+(end-start)/2;
+     int ans=-1;
+     while(start<=end){
+        mid=start+(end-start)/2;
+
+        if(arr[mid]==target){
+            end=mid-1;
+            ans=mid;
+        }
+        if(target>arr[mid]){
+            start=mid+1;
+        }else{
+            end=mid-1;
+        }
+     }
+
+     return ans;
+
+
 }
 
 /*
@@ -19,9 +39,9 @@ int firstOccurence(int arr[],int size,int target){
 int main()
 {
     
-    int arr[]={0,1,2,2,3,4,5};
-    int size=7;
-    int target=2;
+    int arr[]={0,1,2,2,3,4,5,6,7,8,8,9,10};
+    int size=13;
+    int target=8;
     int result=firstOccurence(arr,size,target);
     cout<<result<<endl;
     return 0;
