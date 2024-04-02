4a44f5340ec6490a29a73fbb52b1c734b668c4b2
cmrohityadav <cmrohityadav23@gmail.com>
2024-04-02 18:56:55 +0530

    Array Reverse Inbuilt Methods (Non In-place)

--- Array/C++/Questions/01_reversedArray.cpp
+++ Array/C++/Questions/01_reversedArray.cpp
@@ -1,18 +1,10 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 void reverseArray(int arr[],int size){
-    int start=0;
-    int end=size-1;
-
-    while(start<=end){
-        int temp=arr[start];
-        arr[start]=arr[end];
-        arr[end]=temp;
-        start++;
-        end--;
-    }
-
+    
+    reverse(arr,arr+size);
     for(int i=0;i<size;i++){
         cout<<arr[i]<<" ";
     }
