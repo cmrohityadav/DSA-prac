2f26f224cc8ee8ca940cb0393dcdcb2c1db4a2a1
cmrohityadav <cmrohityadav23@gmail.com>
2024-04-02 18:53:03 +0530

    Array Reverse Using a Loop (In-place)

--- Array/C++/Questions/01_reversedArray.cpp
+++ Array/C++/Questions/01_reversedArray.cpp
@@ -2,14 +2,19 @@
 using namespace std;
 
 void reverseArray(int arr[],int size){
-    int reversedArray[size];
-    for(int i=0;i<size;i++){
-        reversedArray[i]=arr[size-1-i];
+    int start=0;
+    int end=size-1;
 
+    while(start<=end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
     }
 
     for(int i=0;i<size;i++){
-        cout<<reversedArray[i]<<" ";
+        cout<<arr[i]<<" ";
     }
 }
 int main(){
@@ -17,6 +22,7 @@ int main(){
     int originalArray[5]={1,2,3,4,5};
     int size=5;
     reverseArray(originalArray,size);
+    
 
 
     return 0;
