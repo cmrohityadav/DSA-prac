c28374453096f4ed9e1c8daec9fc1cb63b04d350
cmrohityadav <cmrohityadav23@gmail.com>
2024-04-05 17:42:34 +0530

    insertAtTail

---
+++ LinkedList/C++/02_Doubly Linked List/02_insertion.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    Node* prev;
+    int val;
+    Node* next;
+
+    Node(int data){
+        this->val=data;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+
+};
+class DoublyLinkedList{
+    public:
+    Node* head;
+    Node* tail;
+    DoublyLinkedList(){
+        this->head=NULL;
+        this->tail=NULL;
+    }
+    void print(){
+        Node* temp=head;
+        
+        while(temp!=NULL){
+            cout<<temp->val<<" ";
+            temp=temp->next;
+
+        }
+        cout<<endl;
+    }
+
+    
+
+    void insertAtTail(int val){
+        Node* newNode=new Node(val);
+        if(head==NULL){
+            head=newNode;
+            tail=newNode;
+            return;
+        }
+        tail->next=newNode;
+        newNode->prev=tail;
+        tail=newNode;
+        return;
+    }
+};
+
+
+int main(){
+   
+    DoublyLinkedList dll;
+    // cout<<dll.head<<endl;
+    // cout<<dll.tail<<endl;
+
+    dll.insertAtStart(10);
+    dll.insertAtStart(20);
+    dll.insertAtStart(30);
+    dll.print();
+    dll.insertAtTail(40);
+    dll.print();
+    
+
+    return 0;
+}
