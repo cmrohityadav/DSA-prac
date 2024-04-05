811c6fa35989af11a4db9f2c0d133227d25a741e
cmrohityadav <cmrohityadav23@gmail.com>
2024-04-05 18:22:21 +0530

    insertion

--- LinkedList/C++/02_Doubly Linked List/02_insertion.cpp
+++ LinkedList/C++/02_Doubly Linked List/02_insertion.cpp
@@ -60,6 +60,24 @@ class DoublyLinkedList{
         tail=newNode;
         return;
     }
+
+    void insertAtAnyPosition(int val,int position){
+        int count=0;
+        Node* temp=head;
+        while(count!=position-1){
+            temp=temp->next;
+            count++;
+
+        }
+        Node * newNode=new Node(val);
+      
+        newNode->next=temp->next;
+        newNode->prev=temp;
+        temp->next=newNode;
+        temp->next->prev=newNode;
+       
+        return;
+    }
 };
 
 
@@ -75,6 +93,25 @@ int main(){
     dll.print();
     dll.insertAtTail(40);
     dll.print();
+
+    cout<<"insert at tail"<<endl;
+    DoublyLinkedList dll2;
+    dll2.insertAtTail(100);
+    dll2.insertAtTail(200);
+    dll2.insertAtTail(300);
+    dll2.print();
+
+    cout<<"Insert at k position"<<endl;
+    DoublyLinkedList dll3;
+    dll3.insertAtTail(10);
+    dll3.insertAtTail(20);
+    dll3.insertAtTail(30);
+    dll3.insertAtTail(40);
+    dll3.insertAtTail(50);
+    dll3.print();
+    dll3.insertAtAnyPosition(1000,2);
+    dll3.print();
+
     
 
     return 0;
