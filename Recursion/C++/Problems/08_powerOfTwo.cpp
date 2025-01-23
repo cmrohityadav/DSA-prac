16921909593e4b4f917730a3810440958c2c3f7e
cmrohityadav <cmrohityadav23@gmail.com>
2025-01-23 21:41:42 +0530

    solved problem: n Raise To M

--- Recursion/C++/Problems/08_powerOfTwo.cpp
+++ Recursion/C++/Problems/08_powerOfTwo.cpp
@@ -8,10 +8,20 @@ int powerOfTwo(int iNum){
 
     return 2* powerOfTwo(iNum-1);
 }
+
+int nRaiseToM(int n,int m){
+    if(m==1){
+        return n;
+    }
+
+    return n*nRaiseToM(n,m-1);
+}
 int main()
 {
     
 
-    cout<<powerOfTwo(3);
+    cout<<powerOfTwo(3)<<endl;
+
+    cout<<nRaiseToM(3,3);
     return 0;
 }
