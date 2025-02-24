0977787333a8f5773f8188bfe17d538d7d63f864
cmrohityadav <cmrohityadav23@gmail.com>
2025-02-24 23:30:18 +0530

    swapping using 3rd varaible

---
+++ Basic Maths/JS/Problems/03_swapping.js
@@ -0,0 +1,12 @@
+let a=10;
+let b=20;
+
+console.log("Before Swapping ")
+console.log(`a is ${a} and b is ${b}`)
+
+let c=a;
+a=b;
+b=c;
+
+console.log("After Swapping")
+console.log(`a is ${a} and b is ${b}`)
