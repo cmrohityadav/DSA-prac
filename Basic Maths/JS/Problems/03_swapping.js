aa285a64bbcaf5c1d01e14998f56f21e6b2245d1
cmrohityadav <cmrohityadav23@gmail.com>
2025-02-25 23:20:57 +0530

    other way of swapping

--- Basic Maths/JS/Problems/03_swapping.js
+++ Basic Maths/JS/Problems/03_swapping.js
@@ -10,3 +10,27 @@ b=c;
 
 console.log("After Swapping")
 console.log(`a is ${a} and b is ${b}`)
+
+
+// using without 3rd varaible
+let glassOneWaterMl=100;
+let glassTwowaterMl=200;
+console.log("Before Swapping ")
+console.log(`glass A is ${glassOneWaterMl} and B is ${glassTwowaterMl} Ml`)
+
+glassOneWaterMl=glassOneWaterMl+glassTwowaterMl; //one=100+200=300
+glassTwowaterMl=glassOneWaterMl-glassTwowaterMl // two=300-200=100
+glassOneWaterMl=glassOneWaterMl-glassTwowaterMl// one=300-100=200
+
+console.log("After Swapping")
+console.log(`glass A is ${glassOneWaterMl} and B is ${glassTwowaterMl} Ml`)
+
+
+console.log("Before Swapping ")
+let xx=30;
+let yy=60;
+console.log(`x is ${xx} and y is ${yy}`)
+[xx,yy]=[yy,xx]
+console.log("After Swapping")
+console.log(`x is ${xx} and y is ${yy}`)
+
