589eb2e1c7ccf01f0d491bb54464591e58eb0a7e
cmrohityadav <cmrohityadav23@gmail.com>
2025-03-08 23:45:40 +0530

    added basic concept of array

---
+++ Array/JS/01_basic.js
@@ -0,0 +1,16 @@
+
+let arr=[]
+arr.push("TCS")
+arr.push("1520")
+arr.push({"cat":"tech","origin":"india"})
+arr.push(false)
+console.log(arr)
+console.log(`value of elemet at index 1 : ${arr[1]}`)
+
+let arr2=[];
+arr2.push("0")
+arr2.push("1")
+arr2.push("2")
+arr2[8]="Rohit"
+console.log(arr2[4])
+console.log(arr2)
---
+++ Array/JS/2_basicConcept.js
@@ -0,0 +1,19 @@
+/**
+ * In js , no fixed size array 
+ * only dynamic size
+ */
+
+let aar=new Array(3)
+aar[0]=0;
+aar[1]=1;
+aar[2]=2;
+aar[3]=3;
+
+aar[100]=100
+
+console.log(aar)
+
+
+let arr=new Array(4);
+arr.push(5)
+console.log(arr)
