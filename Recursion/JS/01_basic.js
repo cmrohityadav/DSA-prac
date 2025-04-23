9d7296b81d6f9d3a73949c92a9f7cf63e7128d29
cmrohityadav <cmrohityadav23@gmail.com>
2025-04-24 00:00:51 +0530

    changes in basic concept in recursion

--- Recursion/JS/01_basic.js
+++ Recursion/JS/01_basic.js
@@ -1,7 +1,11 @@
 function printNTimes(n){
     if(n==0) return;
-    console.log("Hello World");
+    console.log("Hello World using recursion");
     printNTimes(n-1);
 }
 
-printNTimes(5)
+printNTimes(5);
+
+for(let i=1;i<=5;i++){
+    console.log("Hello World using iteration");
+}
