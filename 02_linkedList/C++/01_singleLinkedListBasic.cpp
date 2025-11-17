0080181c3967aeb7e4d825f20ad6b94a9970bd69
cmrohityadav <cmrohityadav23@gmail.com>
2025-11-17 21:50:26 +0530

    feat: added functionality to insert a node at any position

--- 02_linkedList/C++/01_singleLinkedListBasic.cpp
+++ 02_linkedList/C++/01_singleLinkedListBasic.cpp
@@ -49,6 +49,42 @@ public:
         }
     }
 
+
+    void insertAt(int pos,int val){
+        if(pos<0){
+            cout<<"Please check,...Position can't be Negative\n";
+            cout<<"Position out of range!\n";
+
+            return;
+        }
+        if(pos==0){
+            // push_front(val); //optionally
+            
+            Node* newNode=new Node(val);
+            if(head==NULL){
+                head=newNode;
+                return;
+            }else{
+                newNode->next=head;
+                head=newNode;
+                return;
+            }
+        }
+
+        Node* tempNode=head;
+        for(int i=0;i<pos-1;i++){
+             if(tempNode==NULL){
+                cout<<"Position out of range!\n";
+                return;
+            }
+            tempNode=tempNode->next;
+        }
+
+        Node* newNode=new Node(val);
+        newNode->next=tempNode->next;
+        tempNode->next=newNode;
+    }
+
     void pop_front(){
         if(head==NULL){
             cout<<"SLL is empty\n";
@@ -115,15 +151,25 @@ int main(){
 
     List mySLL;
 
-    mySLL.push_front(10);
+    // mySLL.push_front(10);
     // mySLL.push_front(20);
     // mySLL.push_front(30);
 
-    // mySLL.push_back(100);
-    // mySLL.push_back(200);
+    mySLL.push_back(100);
+    mySLL.push_back(200);
+    mySLL.push_back(300);
+    mySLL.push_back(400);
 
     // mySLL.pop_front();
-    mySLL.pop_back();
+    // mySLL.pop_back();
+
+    // mySLL.insertAt(0,-100);
+    // mySLL.insertAt(5,500);
+
+    // mySLL.insertAt(4,1000);
+    // mySLL.insertAt(9,900);
+
+
 
     mySLL.print();
 
