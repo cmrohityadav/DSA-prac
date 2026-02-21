a711795fb76e29e76561fc3e938f54d4744af721
cmrohityadav <cmrohityadav23@gmail.com>
2026-02-21 11:33:07 +0530

    added Fibonnaci

--- main.cpp
+++ main.cpp
@@ -1,33 +1,32 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
+/*
+* @brief Fibonnaci of n number is f(n)=f(n-1)+f(n-2)
+ * This function returns the nth number in the Fibonacci sequence.
+ * The sequence starts as: 0, 1, 1, 2, 3, 5, 8...
+ * f(0)=0
+ * f(1)=1
+ * f(2)=1
+ * f(3)=2
+ * @param n The position in the Fibonacci sequence (must be >= 0)
+ * @return The nth Fibonacci number
+*/
+int  fibonacci(int n){
+    //base case
+    if(n==0) return 0;
+    if(n==1) return 1;
 
-void fun(vector<int> &a, int n, int idx, vector<int> &tmp) {
-    // Base case
-    if (idx == n) {
-        for (int x : tmp) {
-            cout << x << " ";
-        }
-        cout << endl;
-        return;
-    }
+    //recursion call
+    int n_minus_one=fibonacci(n-1);
+    int n_minus_two=fibonacci(n-2);
 
-    // Not take current element
-    fun(a, n, idx + 1, tmp);
+    // self work
+    return n_minus_one+n_minus_two;
 
-    // Take current element
-    tmp.push_back(a[idx]);
-    fun(a, n, idx + 1, tmp);
-
-    // Backtrack
-    tmp.pop_back();
 }
-
-int main() {
-    vector<int> a = {1, 2, 3};
-    int n = a.size();
-    vector<int> tmp;
-
-    fun(a, n, 0, tmp);
+int main()
+{
+   cout<<fibonacci(3);
 
     return 0;
 }
